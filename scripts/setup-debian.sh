e8d07c604312c683905d6655d8c290a589dd57fb
Glen <glencbz@gmail.com>
2023-08-29 17:30:31 -0700

    add emacs, make steps conditional

--- scripts/setup-debian.sh
+++ scripts/setup-debian.sh
@@ -1,14 +1,29 @@
 #! /usr/bin/env bash
 
-sudo apt install \
-    curl \
-    zsh \
+set -ex
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Basics
+sudo apt-get install curl
 
-# Starship prompt
-curl -sS https://starship.rs/install.sh | sh
+# zsh prompt
+sudo apt-get install zsh
+if ! [[ -e ~/.oh-my-zsh ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # Starship prompt
+    curl -sS https://starship.rs/install.sh | sh
+    cp ../files/.zshrc ~
+fi
 
 # Tmux configs
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if ! [[ -e ~/.tmux.conf ]]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    cp ../files/.tmux.conf ~
+fi
+
+# Spacemacs
+
+if ! which emacs; then
+    sudo apt-get install emacs
+    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+    cp ../files/.spacemacs ~
+fi
